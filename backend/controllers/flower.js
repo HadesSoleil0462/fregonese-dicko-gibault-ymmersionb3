c86e0e8db507744526f9917ae2bbe1030da8186e
Alexandre Gibault <ag-mainmail@protonmail.com>
2023-10-03 11:21:43 +0200

    Add getFolwersByName and flowers/:name route

--- backend/controllers/flower.js
+++ backend/controllers/flower.js
@@ -3,11 +3,36 @@ const data = require("../data.json");
 /**
  * Get all the flowers.
  * @param {*} req 
- * @param {*} res 
+ * @param {*} res All the flowers
  */
 exports.getFlowers = (req, res) => {
     const id = parseInt(req.params.id);
     const flowers = data.flowers;
 
     res.json(flowers);
-}
+};
+
+/**
+ * Get all the flowers by the name.
+ * @param {*} req Name of a flower
+ * @param {*} res flowers corresponding to the name
+ */
+exports.getFlowersByName = async (req, res) => {
+    const name = req.params.name;
+    const flowers = data.flowers;
+    let flowersByNames = [];
+
+    for(let flower of flowers){
+        if(flower.name === name)
+            flowersByNames.push(flower);
+    }
+
+    if(flowersByNames.length === 0){
+        res.status(404).send({message : "Flowers not found"});
+    } else {
+        res.status(200).json({
+            message: "Flowers found succeffully",
+            flowersByNames
+        });
+    }
+};
--- backend/routes/flowers.js
+++ backend/routes/flowers.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/flower");
 
-/**
- * Route to get all the flowers.
- */
-router.get("/flowers", controllers.getFlowers)
+//Route to get all the flowers.
+router.get("/flowers", controllers.getFlowers);
+
+//Route to get all the flowers by the name
+router.get("flowers/:name", controllers.getFlowersByName);
 
 module.exports = router;
