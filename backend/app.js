8b306b6459af4eca93819d221a2683e41cc70806
Alexandre Gibault <ag-mainmail@protonmail.com>
2023-10-04 13:44:42 +0200

    Add customer route, controller and update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 const flowerRoutes = require("./routes/flowers");
+const customerRoutes = require("./routes/customers")
 
 app.use(cors({
     origin: '*'
 }));
 
 app.use(flowerRoutes);
+app.use(customerRoutes);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
