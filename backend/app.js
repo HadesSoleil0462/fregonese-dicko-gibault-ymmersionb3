9420bcea25aefc23674ceaed6cb3000e12417fe5
Alexandre Gibault <ag-mainmail@protonmail.com>
2023-10-04 14:18:31 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,11 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 const flowerRoutes = require("./routes/flowers");
-const customerRoutes = require("./routes/customers")
 
 app.use(cors({
     origin: '*'
 }));
 
 app.use(flowerRoutes);
-app.use(customerRoutes);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
