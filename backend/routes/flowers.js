63200d9eb7a2dce21dd16719fbb4164ae588f8c2
Tom FREGONESE <tom.fregonese@orange.fr>
2023-10-06 08:33:15 +0200

    modification du nom de la route pour accéder à une fleur par son id et par son nom

--- backend/routes/flowers.js
+++ backend/routes/flowers.js
@@ -6,10 +6,10 @@ const controllers = require("../controllers/flower");
 router.get("/flowers", controllers.getFlowers);
 
 //Route to get a flower by the id
-router.get("/flowers/:id", controllers.getFlowerById);
+router.get("/flower/:id", controllers.getFlowerById);
 
 //Route to get all the flowers by the name
-router.get("/flowers/names/:name", controllers.getFlowersByName);
+router.get("/flowers/:name", controllers.getFlowersByName);
 
 //Route to add one flower to the roster.
 router.post("/flowers", controllers.addFlower);
