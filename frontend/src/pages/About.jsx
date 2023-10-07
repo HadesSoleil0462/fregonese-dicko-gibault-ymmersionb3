c2597df8ac140c95b81e726255ad3907649752e4
Alexandre Gibault <ag-mainmail@protonmail.com>
2023-10-07 18:40:34 +0200

    Add frontend missing files

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,43 @@
+import '../styles/About.css'
+
+const About = () => {
+
+    return <div className='history'>
+        <div className='history-detail'>
+            <h3>A propos de Flowers Store</h3>
+            <p>
+            Bienvenue chez Flowers Store, votre destination florale préférée pour des créations florales magnifiques et des cadeaux uniques. Notre passion pour les fleurs se reflète dans chacun de nos arrangements, et nous sommes fiers de vous offrir la fraîcheur, la qualité et la beauté dans chaque bouquet que nous créons.
+            </p>
+        </div>
+        <div className='history-detail'>
+            <h3>Notre histoire</h3>
+            <p>
+            Fondée avec amour, Flowers Store est née de la passion pour la nature et le désir de partager la beauté des fleurs avec le monde. Depuis notre création, nous avons travaillé dur pour devenir la principale destination florale de la région. Notre équipe dévouée et talentueuse de fleuristes s'efforce de créer des arrangements floraux exceptionnels qui évoquent l'émotion et racontent des histoires.
+            </p>
+        </div>
+        <div className='history-detail'>
+            <h3>Nos produits</h3>
+            <p>
+            Chez Flowers Store, vous trouverez une vaste sélection de fleurs fraîches, de plantes d'intérieur, de bouquets de saison, et de cadeaux floraux pour toutes les occasions spéciales de la vie. De la romantique rose rouge aux élégantes orchidées en passant par les vibrants lys et les doux marguerites, notre gamme de produits est conçue pour satisfaire tous les goûts et tous les budgets.
+            </p>
+        </div>
+        <div className='history-detail'>
+            <h3>Engagement envers la qualité</h3>
+            <p>
+            La qualité est au cœur de tout ce que nous faisons. Nous travaillons en étroite collaboration avec les meilleurs fournisseurs de fleurs pour garantir que chaque pétale que nous utilisons est frais et éclatant. Notre équipe de fleuristes expérimentés prend soin de chaque bouquet avec une attention méticuleuse aux détails pour s'assurer qu'il est aussi magnifique que possible.
+            </p>
+        </div>
+        <div className='history-detail'>
+            <h3>Livraison et service client</h3>
+            <p>
+            Nous sommes fiers de notre service de livraison fiable et ponctuel. Lorsque vous commandez chez Flowers Store, vous pouvez avoir l'assurance que votre bouquet sera livré en parfait état et à temps. Notre équipe de service client est également à votre disposition pour répondre à toutes vos questions et vous assister dans le choix du bouquet parfait.
+
+            Chez Flowers Store, nous croyons que les fleurs ont le pouvoir de créer des moments inoubliables et de répandre la joie. Laissez-nous être votre partenaire floral de confiance pour toutes les occasions importantes de votre vie.
+
+            Merci de choisir Flowers Store pour vos besoins floraux. Nous sommes impatients de vous servir et de vous aider à exprimer vos émotions à travers la beauté des fleurs.
+            </p>
+        </div>
+    </div>
+}
+
+export default About;
